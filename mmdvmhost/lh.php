c46a32bf79ddf1eac958cb13c976d1dadb8c4db7
Chipster <go@w.ay>
2022-11-02 09:25:37 -0500

    * Minor: small bug fixes and UI tweaks

--- mmdvmhost/lh.php
+++ mmdvmhost/lh.php
@@ -202,7 +202,15 @@ for ($i = 0;  ($i <= $lastHeardRows - 1); $i++) {
 			    $target = $target;
 			}
 		    }
+
 		    echo "<td align=\"left\">$target</td>";
+
+		if (strpos($listElem[4], "via ")) {
+		    $listElem[4] = preg_replace("/via (.*)/", "<span class='noMob'>via $1</span>", $listElem[4]);
+		}
+		if (strlen($listElem[4]) == 1) { $listElem[4] = str_pad($listElem[4], 8, " ", STR_PAD_LEFT); }
+		if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
+		    echo "<td align=\"left\">$listElem[4]</td>";
 		} else {
 		    if (strlen($listElem[4]) == 1) { $listElem[4] = str_pad($listElem[4], 8, " ", STR_PAD_LEFT); }
 		    if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
