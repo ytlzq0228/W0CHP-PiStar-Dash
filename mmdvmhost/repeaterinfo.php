084f2f8aff26cb179d72e1eb9dd2120c008d6cbd
Chipster <go@w.ay>
2023-01-05 05:51:24 -0600

    * Minor: code cleanups

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -1042,9 +1042,7 @@ if (getConfigItem("NXDN", "RAN", $_SESSION['MMDVMHostConfigs'])) {
 <?php
 	}
     $m17gatewayConfigFile = '/etc/m17gateway';
-    $mmdvmhostConfigFile = '/etc/mmdvmhost';
     if (fopen($m17gatewayConfigFile,'r')) { $configm17gateway = parse_ini_file($m17gatewayConfigFile, true); }
-    $configmmdvm = parse_ini_file($mmdvmhostConfigFile, true);
     $testAPRSdmr = $_SESSION['DMRGatewayConfigs']['APRS']['Enable'];
     $testAPRSysf = $_SESSION['YSFGatewayConfigs']['APRS']['Enable'];
     $testAPRSm17 = $configm17gateway['APRS']['Enable'];
